169e51efae402fc432852668b1ce82058d50eab7
parkmineum <a01087164428@gmail.com>
2024-10-21 02:16:45 +0900

    Initial Commit

---
+++ src/main/java/Spring_CICD_Prac/CICD_Prac/CicdPracApplication.java
@@ -0,0 +1,13 @@
+package Spring_CICD_Prac.CICD_Prac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdPracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdPracApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Spring_CICD_Prac/CICD_Prac/CicdPracApplicationTests.java
@@ -0,0 +1,13 @@
+package Spring_CICD_Prac.CICD_Prac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdPracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
